916f7403012155c01e9015fdd048fa787625c35a
troe5906 <troe5906@CV-BB-5768>
2021-08-19 09:39:55 +0200

    Add project files.

---
+++ EX03ConsolePlay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX03ConsolePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Farven på baggrund tekst
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            //Console størrelse            
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 100;
+            //Spil en lyd første tal=Hz andet tal=millisekunder
+            Console.Beep(9000, 1000);
+
+
+
+
+
+        }
+    }
+}
